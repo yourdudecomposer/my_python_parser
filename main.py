499e6d4d9d29809c0eca90129c48a830455d9763
Filipp Goriainov <barrios1991phil@gmail.com>
2024-02-03 18:49:06 +0400

    make venv

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from driver_config import driver
 from selenium.webdriver.common.by import By
 from logger import logger
+from utils import save_in_viewed
 
 driver.get("https://www.avito.ru/astrahan/avtomobili/do-300000-rubley-ASgCAgECAUXGmgwWeyJmcm9tIjowLCJ0byI6MzAwMDAwfQ?radius=200&s=104&searchRadius=200")
 
 driver.implicitly_wait(0.5)
 urls =  driver.find_elements(By.CSS_SELECTOR, '[data-marker="item-title"]')
 
-
+#/Library/Frameworks/Python.framework/Versions/3.11/bin/python3.11 
 
 for el in urls:
     link = el.get_attribute("href")
--- utils.py
+++ utils.py
@@ -1,6 +1,21 @@
 def save_in_viewed(url):
     print(url)
+
+
+viewed_list = []
+
+#read file
+viewed_list = list(map(str.rstrip, open('viewed.txt').readlines()))
+
+print(viewed_list)
+open('viewed.txt', 'w').write('rrr')
+open('viewed.txt', 'w').write('rrr')
+open('viewed.txt', 'w').write('rrr')
+open('viewed.txt', 'w').write('www')
+        
+        
+def already_saved(url):
+    print(url)
     
     
     
-save_in_viewed(44)
